f882b91cca79ba4adf487f128d1a4f3e7c229d10
EnumaOub <rayane.oubenali@gmail.com>
2024-07-28 22:07:53 +0200

    Add animations when moving on the page

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
-const startAnimation = function(entries, observer) {
+const options = {
+    root: null, // use the document's viewport as the container
+    rootMargin: '0px', // % or px - offsets added to each side of the intersection 
+    threshold: 0.5 // percentage of the target element which is visible
+  }
+
+const startAnimation = function(entries) {
     entries.forEach(entry => {
-        entry.target.classList.toggle("animation", entry.isIntersecting);
-      });
+        if(entry.isIntersecting) {
+			entry.target.classList.add('animate');
+		} else {
+			entry.target.classList.remove('animate');		
+		}
+
+	});
+
 };
 
-const observer = new IntersectionObserver(startAnimation);
-const options = { root: null, rootMargin: '0px', threshold: 1 }; 
+let observer = new IntersectionObserver(startAnimation, options);
 
-const elements = document.querySelectorAll('footer');
-elements.forEach(el => {
-  observer.observe(el, options);
-});
+
+document.querySelectorAll('footer')
+	.forEach(elem => { observer.observe(elem) });
+
+document.querySelectorAll('header')
+	.forEach(elem => { observer.observe(elem) });
+
+document.querySelectorAll('.elem-project')
+	.forEach(elem => { observer.observe(elem) });
